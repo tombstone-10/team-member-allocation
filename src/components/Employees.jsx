bf16bfc4736e168119f399c4b6359b40fab4db22
Ali Ahmed <tombstone.act@gmail.com>
2024-06-15 18:18:16 +0500

    Dynamic Team Header and Footer

--- src/components/Employees.jsx
+++ src/components/Employees.jsx
@@ -1,112 +1,12 @@
-import { useState } from "react";
 import maleProfile from "../images/male.png";
 import femaleProfile from "../images/female.png";
 
-const Employees = () => {
-  const [selectedTeam, setTeam] = useState("TeamA");
-
-  const [employees, setEmployees] = useState([
-    {
-      id: 1,
-      fullName: "Ali Ahmed",
-      designation: "Frontend Developer",
-      gender: "male",
-      teamName: "TeamA",
-    },
-    {
-      id: 2,
-      fullName: "Umar Rasheed",
-      designation: "Backend Developer",
-      gender: "male",
-      teamName: "TeamA",
-    },
-    {
-      id: 3,
-      fullName: "Junaid Tahir",
-      designation: "QA",
-      gender: "male",
-      teamName: "TeamA",
-    },
-    {
-      id: 4,
-      fullName: "Shoaib Ali",
-      designation: "Penetration Tester",
-      gender: "male",
-      teamName: "TeamB",
-    },
-    {
-      id: 5,
-      fullName: "David Henry",
-      designation: "DotNet Developer",
-      gender: "male",
-      teamName: "TeamB",
-    },
-    {
-      id: 6,
-      fullName: "Sarah Blake",
-      designation: "SQL Server DBA",
-      gender: "female",
-      teamName: "TeamB",
-    },
-    {
-      id: 7,
-      fullName: "James Bennet",
-      designation: "Angular Developer",
-      gender: "male",
-      teamName: "TeamC",
-    },
-    {
-      id: 8,
-      fullName: "Jessica Faye",
-      designation: "API Developer",
-      gender: "female",
-      teamName: "TeamC",
-    },
-    {
-      id: 9,
-      fullName: "Lita Stone",
-      designation: "C++ Developer",
-      gender: "female",
-      teamName: "TeamC",
-    },
-    {
-      id: 10,
-      fullName: "Daniel Young",
-      designation: "Python Developer",
-      gender: "male",
-      teamName: "TeamD",
-    },
-    {
-      id: 11,
-      fullName: "Adrian Jacobs",
-      designation: "Vue Developer",
-      gender: "male",
-      teamName: "TeamD",
-    },
-    {
-      id: 12,
-      fullName: "Devin Monroe",
-      designation: "Graphic Designer",
-      gender: "male",
-      teamName: "TeamD",
-    },
-  ]);
-
-  function handleTeamSelectionChange(event) {
-    setTeam(event.target.value);
-  }
-
-  function handleEmployeeCardClick(event) {
-    const transformedEmployees = employees.map((employee) =>
-      employee.id === parseInt(event.currentTarget.id)
-        ? employee.teamName === selectedTeam
-          ? { ...employee, teamName: "" }
-          : { ...employee, teamName: selectedTeam }
-        : employee
-    );
-    setEmployees(transformedEmployees);
-  }
-
+const Employees = ({
+  employees,
+  selectedTeam,
+  handleEmployeeCardClick,
+  handleTeamSelectionChange,
+}) => {
   return (
     <main className="container">
       <div className="row justify-content-center mt-3 mb-3">
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,16 @@
-const Header = () => {
+const Header = ({ selectedTeam, teamMemberCount }) => {
   return (
-    <header>
-      <h1>Header</h1>
+    <header className="container">
+      <div className="row justify-content-center mt-3 mb-4">
+        <div className="col-8">
+          <h2>Team Member Allocation</h2>
+          <h4 className="fw-normal">
+            {teamMemberCount === 1
+              ? `${selectedTeam} has ${teamMemberCount} member.`
+              : `${selectedTeam} has ${teamMemberCount} members.`}
+          </h4>
+        </div>
+      </div>
     </header>
   );
 };
