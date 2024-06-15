51fd171ac0c2eb244b78f2db97decd17aff5dac3
Ali Ahmed <tombstone.act@gmail.com>
2024-06-15 05:41:46 +0500

    Employees Data Added

--- src/components/Employees.jsx
+++ src/components/Employees.jsx
@@ -1,9 +1,94 @@
+import { useState } from "react";
+
 const Employees = () => {
-  return (
-    <main>
-      <h1>Employee</h1>
-    </main>
-  );
+  const [employees, setEmployees] = useState([
+    {
+      id: 1,
+      fullName: "Ali Ahmed",
+      designation: "Frontend Developer",
+      gender: "male",
+      teamName: "TeamA",
+    },
+    {
+      id: 2,
+      fullName: "Umar Rasheed",
+      designation: "Backend Developer",
+      gender: "male",
+      teamName: "TeamA",
+    },
+    {
+      id: 3,
+      fullName: "Junaid Tahir",
+      designation: "QA",
+      gender: "male",
+      teamName: "TeamA",
+    },
+    {
+      id: 4,
+      fullName: "Shoaib Ali",
+      designation: "Penetration Tester",
+      gender: "male",
+      teamName: "TeamB",
+    },
+    {
+      id: 5,
+      fullName: "David Henry",
+      designation: "DotNet Developer",
+      gender: "male",
+      teamName: "TeamB",
+    },
+    {
+      id: 6,
+      fullName: "Sarah Blake",
+      designation: "SQL Server DBA",
+      gender: "female",
+      teamName: "TeamB",
+    },
+    {
+      id: 7,
+      fullName: "James Bennet",
+      designation: "Angular Developer",
+      gender: "male",
+      teamName: "TeamC",
+    },
+    {
+      id: 8,
+      fullName: "Jessica Faye",
+      designation: "API Developer",
+      gender: "female",
+      teamName: "TeamC",
+    },
+    {
+      id: 9,
+      fullName: "Lita Stone",
+      designation: "C++ Developer",
+      gender: "female",
+      teamName: "TeamC",
+    },
+    {
+      id: 10,
+      fullName: "Daniel Young",
+      designation: "Python Developer",
+      gender: "male",
+      teamName: "TeamD",
+    },
+    {
+      id: 11,
+      fullName: "Adrian Jacobs",
+      designation: "Vue Developer",
+      gender: "male",
+      teamName: "TeamD",
+    },
+    {
+      id: 12,
+      fullName: "Devin Monroe",
+      designation: "Graphic Designer",
+      gender: "male",
+      teamName: "TeamD",
+    },
+  ]);
+
+  return <main></main>;
 };
 
 export default Employees;
